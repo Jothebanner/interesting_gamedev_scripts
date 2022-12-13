aab02eb9e1d50fe9ba81f72ca77bc7b23bb05dea
Jothebanner <hawks.jacob.g@gmail.com>
2022-12-12 23:53:05 -0700

    Add files via upload

---
+++ FOVDetection.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FOVDetection : MonoBehaviour
+{
+    public float maxAngle;
+    public float maxRadius;
+    public string enemyTag;
+
+    private GameObject closestEnemy;
+
+    void Update()
+    {
+
+    }
+
+    public GameObject FindClosestEnemy()
+    {
+        float distanceToClosestEnemy = Mathf.Infinity;
+        closestEnemy = null;
+        GameObject[] allEnemies = GameObject.FindGameObjectsWithTag(enemyTag);
+
+		if (allEnemies.Length > 0)
+		{
+			foreach (GameObject currentEnemy in allEnemies)
+			{
+				float distanceToEnemy = (currentEnemy.transform.position - this.transform.position).sqrMagnitude;
+				if (distanceToEnemy < distanceToClosestEnemy)
+				{
+					distanceToClosestEnemy = distanceToEnemy;
+					closestEnemy = currentEnemy;
+				}
+			}
+		}
+        return closestEnemy;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.white;
+        Gizmos.DrawWireSphere(transform.position, maxRadius);
+
+        //Vector3 fovLine1 = Quaternion.AngleAxis(maxAngle, transform.forward) * transform.up * maxRadius;
+        //Vector3 fovLine2 = Quaternion.AngleAxis(-maxAngle, transform.forward) * transform.up * maxRadius;
+
+        //Gizmos.color = Color.blue;
+        //Gizmos.DrawRay(transform.position, fovLine1);
+        //Gizmos.DrawRay(transform.position, fovLine2);
+
+        Gizmos.color = Color.red;
+		if (closestEnemy)
+			Gizmos.DrawRay(transform.position, (closestEnemy.transform.position - transform.position).normalized * maxRadius);
+    }
+
+    public bool inFOV(Transform checkingObject, Transform target)
+    {
+        if ((target.position - checkingObject.position).magnitude < maxRadius)
+        {
+            return true;
+        }
+
+        return false;
+    }
+}
---
+++ ZombieController.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class ZombieController : Mirror.NetworkBehaviour
+{
+	private Animator animator;
+	private Rigidbody2D rb;
+	private bool targetSighted;
+	private bool attack;
+	private GameObject closestEnemy;
+
+	public float speed;
+	public float health;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		animator = GetComponentInChildren<Animator>();
+		rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (!isServer)
+		{
+			return;
+		}
+
+		closestEnemy = GetComponentInChildren<FOVDetection>().FindClosestEnemy();
+
+		if (closestEnemy && GetComponentInChildren<FOVDetection>().inFOV(transform, closestEnemy.transform))
+		{
+			targetSighted = true;
+		}
+		else
+		{
+			targetSighted = false;
+		}
+
+		if (closestEnemy)
+		{
+			if ((transform.position - closestEnemy.transform.position).magnitude <= 2)
+			{
+				animator.SetBool("Attack", true);
+				attack = true;
+			}
+			else
+			{
+				animator.SetBool("Attack", false);
+				attack = false;
+			}
+		}
+		else
+		{
+			animator.SetBool("Attack", false);
+			attack = false;
+		}
+	}
+
+    private void FixedUpdate()
+    {
+		if (!targetSighted && !attack)
+		{
+			rb.velocity = new Vector2(speed, 0) * Time.deltaTime;
+			animator.SetFloat("horSpeed", rb.velocity.x);
+		}
+		else if (targetSighted && closestEnemy && !attack)
+		{
+			Vector2 direction = closestEnemy.transform.position - transform.position;
+			float total = Mathf.Abs(direction.x) + Mathf.Abs(direction.y);
+			float xPercent = direction.x / total;
+			float yPercent = direction.y / total;
+			Vector2 speedTowardsObject = new Vector2(xPercent, yPercent) * speed * 2 * Time.deltaTime;
+			rb.velocity = speedTowardsObject;
+			animator.SetFloat("horSpeed", rb.velocity.x);
+		}
+		else if (attack)
+		{
+			rb.velocity = new Vector2(0, 0);
+			animator.SetFloat("horSpeed", rb.velocity.x);
+		}
+	}
+
+	public void die()
+	{
+		Destroy(this.gameObject);
+	}
+
+	public void TakeDamage(float damage)
+	{
+		health -= damage;
+		if (health <= 0)
+		{
+			die();
+		}
+	}
+}
